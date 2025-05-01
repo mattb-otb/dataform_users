e1f1fcb19419aa1b54a5e956ee2eb31a7a42559b
matthew bennett <matthew.bennett@onthebeach.co.uk>
2025-05-01 17:15:10 +0000

    added loads of stuff

--- includes/cohorts_udf.js
+++ includes/cohorts_udf.js
@@ -3,14 +3,20 @@
 function get_cohort_name(cohort) {
     return `
 CASE
+    WHEN ${cohort} = -4 THEN '-4. Dormant Top Customer'
+    WHEN ${cohort} = -3 THEN '-3. Dormant Repeat Customer'
+    WHEN ${cohort} = -2 THEN '-2. Dormant New Customer'
+    WHEN ${cohort} = -1 THEN '-1. Dormant Browser'
     WHEN ${cohort} = 1 THEN '1. Unengaged Browser'
     WHEN ${cohort} = 2 THEN '2. Engaged Browser'
-    WHEN ${cohort} = 3 THEN '3. Unengaged New Customer'
-    WHEN ${cohort} = 4 THEN '4. New Customer'
+    WHEN ${cohort} = 3 THEN '4. Unengaged New Customer'
+    WHEN ${cohort} = 4 THEN '3. New Customer'
     WHEN ${cohort} = 5 THEN '5. Re-engaged New Customer'
-    WHEN ${cohort} = 6 THEN '6. Unengaged Repeat Customer'
-    WHEN ${cohort} = 7 THEN '7. Repeat Customer'
+    WHEN ${cohort} = 6 THEN '7. Unengaged Repeat Customer'
+    WHEN ${cohort} = 7 THEN '6. Repeat Customer'
     WHEN ${cohort} = 8 THEN '8. Re-engaged Repeat Customer'
+    WHEN ${cohort} = 9 THEN '9. Unengaged Top Customer'
+    WHEN ${cohort} = 10 THEN '10. Top Customer'
 ELSE '0. Unknown' END    
 `;
 };
--- includes/constants.js
+++ includes/constants.js
@@ -10,8 +10,16 @@ const DATAFORM_LV1 = 'dataform_users_bronze';
 const DATAFORM_LV2 = 'dataform_users_silver';
 const DATAFORM_LV3 = 'dataform_users_gold';
 
+//* Project variables. *//
+const ATT_SETTINGS = (data) => {
+  const ENGAGED_SESSION_MODEL = `(deal_search_events >= 1 OR pageviews >= 2 OR login_events > 0 OR create_account_events > 0 OR order_count > 0)`;
+  const RETURN_HOME_DAY_DELAY = 1;
+  const DORMANT_USER_TIME = 90;
+};
+
 //* Exported modules are below. *//
 module.exports = {
-    PROJECT_NAME, GA_DATAFORM, GA_MI_GOLD, //* GCP project. *//
-    DATAFORM_LV1, DATAFORM_LV2, DATAFORM_LV3 //* Dataform levels/datasets/schema. *//
+PROJECT_NAME, GA_DATAFORM, GA_MI_GOLD, //* GCP project. *//
+DATAFORM_LV1, DATAFORM_LV2, DATAFORM_LV3, //* Dataform levels/datasets/schema. *//
+ATT_SETTINGS //* Attribution settings. *//
 }
